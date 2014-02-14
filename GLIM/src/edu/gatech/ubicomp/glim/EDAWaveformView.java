735be6e4d464cb60491e3714051601b1012381ee
Aman Parnami <amanparnami@gmail.com>
2014-02-13 21:58:34 -0500

    Removed unused code.

--- GLIM/src/edu/gatech/ubicomp/glim/EDAWaveformView.java
+++ GLIM/src/edu/gatech/ubicomp/glim/EDAWaveformView.java
@@ -88,35 +88,6 @@ public class EDAWaveformView extends SurfaceView {
 //    	getHolder().unlockCanvasAndPost(canvas);
 //    }
   }
-
-  /**
-   * Updates the waveform view with a new "frame" of samples and renders it.
-   * The new frame gets added to the front of the rendering queue, pushing the
-   * previous frames back, causing them to be faded out visually.
-   * 
-   * @param buffer the most recent buffer of audio samples.
-   */
-  public synchronized void updateAudioData(int[] buffer) {
-    int[] newBuffer;
-
-    // We want to keep a small amount of history in the view to provide a nice
-    // fading effect. We use a linked list that we treat as a queue for this.
-    if (mAudioData.size() == HISTORY_SIZE) {
-      newBuffer = mAudioData.removeFirst();
-      System.arraycopy(buffer, 0, newBuffer, 0, buffer.length);
-    } else {
-      newBuffer = buffer.clone();
-    }
-
-    mAudioData.addLast(newBuffer);
-
-    // Update the display.
-    Canvas canvas = getHolder().lockCanvas();
-    if (canvas != null) {
-      drawWaveform(canvas);
-      getHolder().unlockCanvasAndPost(canvas);
-    }
-  }
   
   public synchronized void updateEDADataSimple(Float newValue) {
 	  //Put the new value in the queue
@@ -235,54 +206,6 @@ public class EDAWaveformView extends SurfaceView {
      
 
       //brightness += colorDelta;
-    }
-  }
-
-  /**
-   * Repaints the view's surface.
-   * 
-   * @param canvas the {@link Canvas} object on which to draw.
-   */
-  private void drawWaveform(Canvas canvas) {
-    // Clear the screen each time because SurfaceView won't do this for us.
-    canvas.drawColor(Color.BLACK);
-
-    float width = getWidth();
-    float height = getHeight();
-    float centerY = height / 2;
-
-    // We draw the history from oldest to newest so that the older audio
-    // data is further back and darker than the most recent data.
-    int colorDelta = 255 / (HISTORY_SIZE + 1);
-    int brightness = colorDelta;
-
-    for (int[] buffer : mAudioData) {
-      //mPaint.setColor(Color.argb(brightness, 128, 255, 192));
-    mPaint.setARGB(120, 255, 0, 0);	
-    	
-      float lastX = -1;
-      float lastY = -1;
-
-      // For efficiency, we don't draw all of the samples in the buffer,
-      // but only the ones that align with pixel boundaries.
-      for (int x = 0; x < width; x=x+5) {
-        int index = (int) ((x / width) * buffer.length);
-        int sample = buffer[index];
-//        float y = (sample / MAX_AMPLITUDE_TO_DRAW) * centerY + centerY;
-
-    	  float y = (sample) + centerY;
-    	  
-        if (lastX != -1) {
-          //canvas.drawLine(lastX, lastY, x, y, mPaint);
-        	canvas.drawCircle(x, y, 5, mPaint);
-        	
-        }
-        
-        lastX = x;
-        lastY = y;
-      }
-
-      brightness += colorDelta;
     }
   }
 }
