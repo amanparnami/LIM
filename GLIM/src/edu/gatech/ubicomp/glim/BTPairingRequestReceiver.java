18d11bb6693672fcada07e669e7cf6ec017ccda4
Aman Parnami <amanparnami@gmail.com>
2014-02-21 23:39:33 -0500

    Using AIDL to do pairing.

--- GLIM/src/edu/gatech/ubicomp/glim/BTPairingRequestReceiver.java
+++ GLIM/src/edu/gatech/ubicomp/glim/BTPairingRequestReceiver.java
@@ -5,10 +5,13 @@ import java.lang.reflect.Method;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.IBluetooth;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.util.Log;
 
 public class BTPairingRequestReceiver extends BroadcastReceiver {
@@ -24,40 +27,48 @@ public class BTPairingRequestReceiver extends BroadcastReceiver {
 		if(deviceMacId == GlimMainActivity.BT_MAC_ADD_FOR_EDA_SENSOR) {
 			pinString = EDA_PIN;
 		} else {
-			
+
 			pinString = HRM_PIN;
 		}
 		Log.d("BTIntent", b.get("android.bluetooth.device.extra.DEVICE")
 				.toString());
 		Log.d("BTIntent",
 				b.get("android.bluetooth.device.extra.PAIRING_VARIANT")
-						.toString());
+				.toString());
+
+		IBluetooth ib =getIBluetooth();
 		try {
-			BluetoothDevice device = BluetoothAdapter.getDefaultAdapter().getRemoteDevice(deviceMacId);
-			Method m = BluetoothDevice.class.getMethod("convertPinToBytes",
-					new Class[] { String.class });
-			byte[] pin = (byte[]) m.invoke(device, pinString);
-			m = device.getClass().getMethod("setPin",
-					new Class[] { pin.getClass() });
-			Object result = m.invoke(device, pin);
-			Log.d("BTTest", result.toString());
-			
-			Log.d("Bond state", "BOND_STATED = " + device.getBondState());
-		} catch (SecurityException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (NoSuchMethodException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
+			ib.setPin(deviceMacId,pinString.getBytes());
+		} catch (RemoteException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	private IBluetooth getIBluetooth() {
+		IBluetooth ibt = null;
+
+		try {
+
+			Class c2 = Class.forName("android.os.ServiceManager");
+
+			Method m2 = c2.getDeclaredMethod("getService",String.class);
+			IBinder b = (IBinder) m2.invoke(null, "bluetooth");
+
+			Class c3 = Class.forName("android.bluetooth.IBluetooth");
+
+			Class[] s2 = c3.getDeclaredClasses();
+
+			Class c = s2[0];
+			Method m = c.getDeclaredMethod("asInterface",IBinder.class);
+			m.setAccessible(true);
+			ibt = (IBluetooth) m.invoke(null, b);
+
+
+		} catch (Exception e) {
+			Log.e("flowlab", "Erroraco!!! " + e.getMessage());
+		}
+
+		return ibt;
 	}
 }
